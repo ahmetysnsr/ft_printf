75c74d8bd32732db7ab34251f67aac53f7fb5bc5
Ahmet Yasin Sari <asari@k2m15s09.42kocaeli.com.tr>
2025-08-04 19:26:42 +0300

    HELLO

---
+++ ft_printf.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asari <asari@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/13 15:16:10 by asari             #+#    #+#             */
+/*   Updated: 2025/07/21 22:35:05 by asari            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <unistd.h>
+
+int	ft_printf(const char *format, ...);
+
+int	ft_putchar(char c);
+int	ft_putstr(char *str);
+int	ft_putnbr(int n);
+int	ft_putnbr_unsigned(unsigned int n);
+int	ft_putnbr_hex(unsigned int n, int uppercase);
+int	ft_putptr(void *ptr);
+
+#endif
---
+++ ft_putptr.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putptr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asari <asari@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/13 13:25:11 by asari             #+#    #+#             */
+/*   Updated: 2025/08/04 19:22:31 by asari            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+static int	ft_putnbr_hex_ptr(size_t n)
+{
+	char	*base;
+	int		count;
+	int		temp;
+
+	base = "0123456789abcdef";
+	count = 0;
+	if (n >= 16)
+	{
+		temp = ft_putnbr_hex_ptr(n / 16);
+		if (temp == -1)
+			return (-1);
+		count += temp;
+	}
+	temp = ft_putchar(base[n % 16]);
+	if (temp == -1)
+		return (-1);
+	count += temp;
+	return (count);
+}
+
+int	ft_putptr(void *ptr)
+{
+	int	count;
+
+	count = 0;
+	if (!ptr)
+		count += ft_putstr("(nil)");
+	else
+	{
+		count += ft_putstr("0x");
+		if (count == -1)
+			return (-1);
+		count += ft_putnbr_hex_ptr((size_t)ptr);
+		if (count == 1)
+			return (-1);
+	}
+	return (count);
+}
